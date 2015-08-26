b88c2939e505e86fdcad7c02568e6b54cf4ec9f3
Nyaundi Brian <ndieksman@gmail.com>
2015-08-26 14:33:39 +0300

    add libs and config

---
+++ config/config.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: brian
+ * Date: 6/23/15
+ * Time: 4:47 PM
+ */
+
+#defined(__ROOT__)?null:define('__ROOT__', dirname(__DIR__));
+
+if (!defined('DB_HOST')){define('DB_HOST', getenv('OPENSHIFT_MYSQL_DB_HOST'));}
+if (!defined('DB_PASSWORD')){define('DB_PASSWORD', getenv('OPENSHIFT_MYSQL_DB_PASSWORD'));}
+if (!defined('DB_NAME')){define('DB_NAME', getenv('OPENSHIFT_GEAR_NAME'));}
+if (!defined('DB_USER')){define('DB_USER', getenv('OPENSHIFT_MYSQL_DB_USERNAME'));}
+
+
+?>
+
