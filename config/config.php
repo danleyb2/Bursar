02eb18835083e250102e3935652bae292cb9c22a
Nyaundi Brian <ndieksman@gmail.com>
2015-08-26 14:39:47 +0300

    libs

--- config/config.php
+++ config/config.php
@@ -8,10 +8,10 @@
 
 #defined(__ROOT__)?null:define('__ROOT__', dirname(__DIR__));
 
-if (!defined('DB_HOST')){define('DB_HOST', getenv('OPENSHIFT_MYSQL_DB_HOST'));}
-if (!defined('DB_PASSWORD')){define('DB_PASSWORD', getenv('OPENSHIFT_MYSQL_DB_PASSWORD'));}
-if (!defined('DB_NAME')){define('DB_NAME', getenv('OPENSHIFT_GEAR_NAME'));}
-if (!defined('DB_USER')){define('DB_USER', getenv('OPENSHIFT_MYSQL_DB_USERNAME'));}
+if (!defined('DB_HOST')){define('DB_HOST', 'localhost');}
+if (!defined('DB_PASSWORD')){define('DB_PASSWORD', 'brian123');}
+if (!defined('DB_NAME')){define('DB_NAME', 'bursar');}
+if (!defined('DB_USER')){define('DB_USER', 'brian');}
 
 
 ?>
